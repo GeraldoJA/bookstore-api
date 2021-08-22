ec038bf967072a823878b50046f2949130e2242c
Geraldo <geraldo.gja@gmail.com>
2021-08-21 23:09:11 -0300

    findAll para Categoria

--- src/main/java/com/Geraldo/bookstore/service/CategoriaService.java
+++ src/main/java/com/Geraldo/bookstore/service/CategoriaService.java
@@ -1,11 +1,13 @@
 package com.Geraldo.bookstore.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.Geraldo.bookstore.domain.Categoria;
+import com.Geraldo.bookstore.dtos.CategoriaDTO;
 import com.Geraldo.bookstore.exceptions.ObjectNotFoundException;
 import com.Geraldo.bookstore.repositories.CategoriaRepository;
 
@@ -19,5 +21,9 @@ public class CategoriaService {
 		Optional<Categoria> obj = repository.findById(id);
 		return obj.orElseThrow( () -> new ObjectNotFoundException(
 				"Objeto não encontrato! Id: " + id + ", Tipo: " + Categoria.class.getName() ) );
+	}
+	
+	public List<Categoria> findAll() {
+		return repository.findAll();
 	}
 }
